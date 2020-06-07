30652469b2f9fb9a3e17f48b15683fab6f50eee5
Xinyu Qu <quxinyuu@hotmail.com>
2020-06-06 23:50:09 -0400

    Initial Commit

---
+++ miniProgram/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  miniProgram
+//
+//  Created by Xinyu Qu on 6/6/20.
+//  Copyright © 2020 Xinyu Qu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
