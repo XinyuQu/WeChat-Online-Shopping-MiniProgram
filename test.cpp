9f6aea3600757874d5ceafda76b4e8ca4e032c2e
XinyuQu <quxinyuu@hotmail.com>
2020-07-19 22:51:09 -0400

    update test

--- test.cpp
+++ test.cpp
@@ -9,7 +9,7 @@ int main()
     //连接数据库
     db.initDB("cdb-54f0tuve.cd.tencentcdb.com","root","R23BN4xO1KGqZBe7SEpufaLNX","test",10092);
     //将用户信息添加到数据库
-    db.exeSQL("INSERT INTO test values('xq','123');");
+    db.exeSQL("INSERT INTO test (c1,c2) values('xq','123');");
     db.exeSQL("INSERT INTO test values('xqq','456');");
     //将所有用户信息读出，并输出。
     db.exeSQL("SELECT * from test;");
