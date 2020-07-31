e6a4982ba86e58ce5b4cea8cf9e58bbb06b7ce6b
XinyuQu <quxinyuu@hotmail.com>
2020-07-31 15:43:25 -0400

    update makefile

--- makefile
+++ makefile
@@ -1,2 +1,3 @@
 test:test.cpp MyDB.cpp  	
 	g++  test.cpp MyDB.cpp -o test  `mysql_config --cflags --libs` 
+	#g++ -I/usr/local/mysql/include -o  test test.cpp MyDB.cpp -lmysqlclient
--- test.cpp
+++ test.cpp
@@ -9,6 +9,7 @@ int main()
     //连接数据库
     db.initDB("cdb-54f0tuve.cd.tencentcdb.com","root","R23BN4xO1KGqZBe7SEpufaLNX","test",10092);
     //将用户信息添加到数据库
+    // db.exeSQL("INSERT INTO ");
     db.exeSQL("INSERT INTO test values('xq');");
     db.exeSQL("INSERT INTO test values('xqq');");
     //将所有用户信息读出，并输出。
